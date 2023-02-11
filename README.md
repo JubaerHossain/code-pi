eb5f1346468700a8fe821a3baadbb7d1b7cdda5c
JubaerHossain <jubaer01.cse@gmail.com>
2023-02-11 12:46:45 +0600

    s

---
+++ README.md
@@ -0,0 +1,36 @@
+**Code-Pi**
+A Visual Studio Code extension that uses OpenAI's API to provide code suggestions.
+
+**Features**
+As you type, Code-Pi will suggest code snippets based on OpenAI's API.
+Works with any programming language supported by OpenAI's API.
+
+
+**Installation**
+    - Open Visual Studio Code.
+    - Open the Command Palette (Ctrl+Shift+P or Cmd+Shift+P).
+    - Type ext install in the Command Palette, then search for Code-Pi.
+    - Click the Install button to install the extension.
+    - Once installed, click the Reload button to reload Visual Studio Code after the extension has been installed.
+**Usage**
+    - Open Visual Studio Code.
+    - Create a new file or open an existing file in Visual Studio Code.
+    - Start typing and Code-Pi will suggest code snippets based on what you type.
+    - Select a suggestion and it will be inserted into your code.
+**Limitations**
+    - Code-Pi is currently in beta and may contain bugs.
+    - The suggestions provided by OpenAI's API may not always be relevant or accurate.
+    - The extension may have performance issues if the API Key is used excessively.
+**Known Issues**
+None at this time.
+**Release Notes**
+0.0.1
+Initial release of Code-Pi
+**License**
+Code-Pi is released under the MIT License.
+
+**Contributing**
+Code-Pi is an open source project and contributions are welcome! If you'd like to contribute, please fork the repository and create a pull request.
+
+
+
---
+++ extension.js
@@ -0,0 +1,78 @@
+const vscode = require('vscode');
+const axios = require('axios');
+
+const apiUrl = 'https://api.openai.com/v1/engines/autocomplete/jobs';
+const apiKey = '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';
+
+async function getCompletions(query) {
+  const response = await axios.post(apiUrl, {
+    prompt: query,
+    "model": "code-davinci-002",
+    "max_tokens": 7,
+    "temperature": 0
+  }, {
+    headers: {
+      'Authorization': `Bearer ${apiKey}`
+    }
+  });
+  const completions = response.data.choices[0].text.split("\n");
+  return completions;
+}
+
+class CompletionProvider {
+  constructor() {}
+
+  provideCompletionItems(
+    document,
+    position,
+    token,
+    context
+  ) {
+    const line = document.lineAt(position).text;
+    return getCompletions(line).then((completions) => {
+      return completions.map((completion) => {
+        const completionItem = new vscode.CompletionItem(completion);
+        completionItem.insertText = completion;
+        return completionItem;
+      });
+    });
+  }
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "code-pi" is now active!');
+
+	const provider = new CompletionProvider();
+	const disposable = vscode.languages.registerCompletionItemProvider(
+		'*',
+		provider,
+		'.'
+	);
+	context.subscriptions.push(disposable);
+
+	let codePIDisposable = vscode.commands.registerCommand('code-pi.codePI', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Welcome to Code PI!');
+	});
+
+	context.subscriptions.push(codePIDisposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
